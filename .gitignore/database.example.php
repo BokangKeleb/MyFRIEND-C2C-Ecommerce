9c37a7a0604ceceece301f10a80ca8fc9f366bf8
BokangKeleb <EDUV4964814@vossie.net>
2026-06-18 15:03:16 +0200

    Add example database configuration to .gitignore

---
+++ .gitignore/database.example.php
@@ -0,0 +1,18 @@
+<?php
+
+$servername = "YOUR_DATABASE_HOST";
+$username = "YOUR_DATABASE_USERNAME";
+$password = "YOUR_DATABASE_PASSWORD";
+$database = "YOUR_DATABASE_NAME";
+
+$conn = mysqli_connect(
+    $servername,
+    $username,
+    $password,
+    $database
+);
+
+if (!$conn) {
+    die("Database connection failed: " . mysqli_connect_error());
+}
+?>
